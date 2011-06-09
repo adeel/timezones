f00a8d81da80ba71d655e6078ccb6ee297f3b233
Adeel Ahmad Khan <adeel@adeel.ru>
2011-06-09 11:36:46 -0400

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name="timezones",
+  version="0.1",
+  author="Adeel Ahmad Khan",
+  author_email="adeel@adeel.ru",
+  py_modules=["timezones"],
+  install_requires=["pytz"],
+  license="BSD")
